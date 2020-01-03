0088dbcfb26a199b04a361ed0818c4123a703683
Lincoln Simmons <lincolnsimmons@gmail.com>
2020-01-02 23:16:20 -0700

    Modularizing the hello_world function to a hello_world module
    - Learning modules as a way to modularize code into different files
    - hello_world() becomes hello_world::run()
    - Added a guessing_game module I wrote with the Rust Book

---
+++ src/guessing_game.rs
@@ -0,0 +1,36 @@
+use rand::random;
+use std::cmp::Ordering;
+use std::io;
+use std::io::Write;
+
+pub fn run() {
+    let secret: u8 = random();
+
+    println!("Random number generated...");
+
+    loop {
+        print!("Guess a number: ");
+        std::io::stdout().flush().unwrap();
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input!");
+                continue;
+            },
+        };
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("You guess correctly!");
+                break;
+            },
+        }
+    }
+}
